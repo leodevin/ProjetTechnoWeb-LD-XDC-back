1e369752ed6c40016d112a2df29df6ad84774a2d
leonardev <leonard.devincre@free.fr>
2019-12-06 14:01:17 +0100

    Ajout de tous les modeles

---
+++ models/Measure.jsx
@@ -0,0 +1,23 @@
+
+// grab the things we need
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create a schema
+const measureSchema = new Schema({
+    _id: ObjectId,
+    type: String,
+    creationDate: Date,
+    sensorID: ObjectId,
+    value: Number
+});
+
+// the schema is useless so far
+// we need to create a model using it
+const Measure = mongoose.model('Measure', measureSchema);
+
+// make this available to our users in our Node applications
+module.exports = Measure;
+
+
+// Ressources : https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
---
+++ models/Sensor.jsx
@@ -0,0 +1,22 @@
+
+// grab the things we need
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create a schema
+const sensorSchema = new Schema({
+    _id: ObjectId,
+    creationDate: Date,
+    location: String,
+    userID: ObjectId
+});
+
+// the schema is useless so far
+// we need to create a model using it
+const Sensor = mongoose.model('Sensor', sensorSchema);
+
+// make this available to our users in our Node applications
+module.exports = Sensor;
+
+
+// Ressources : https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
