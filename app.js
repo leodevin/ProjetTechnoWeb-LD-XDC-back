fd64c64d17bdab4ab04a796632f55ec05802014a
leonardev <leonard.devincre@free.fr>
2019-12-07 16:09:11 +0100

    Back pas encore fonctionnel

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
 var express = require("express");
 var path = require("path");
+let cors = require('cors');
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var usersRouter = require("./routes/usersRouter");
 
 var app = express();
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/myappdatabase');
+mongoose.connect('mongodb://localhost:27017/DashboardProject', {useNewUrlParser: true});
 
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
+db.once('open', () => console.log('connected to the fucking  database'));
+// checks if connection with the database is successful
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-});
+
+
+ app.use("/", indexRouter);
+ app.use("/users", usersRouter);
+
 
 module.exports = app;
--- models/User.jsx
+++ models/User.jsx
@@ -2,6 +2,7 @@
 // grab the things we need
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+var ObjectId = mongoose.Schema.Types.ObjectId;
 
 // create a schema
 const userSchema = new Schema({
@@ -9,15 +10,13 @@ const userSchema = new Schema({
     location: String,
     personsInHouse: Number,
     houseSize: String
-
 });
 
 // the schema is useless so far
 // we need to create a model using it
-const User = mongoose.model('User', userSchema);
 
 // make this available to our users in our Node applications
-module.exports = User;
+module.exports = mongoose.model('User', userSchema, 'User');
 
 
 // Ressources : https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
